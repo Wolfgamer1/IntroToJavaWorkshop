d334d739de6142718b0599e3efe6a5bc67fc1cc6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-12 17:49:41 -0800

    triangle shell flaming ninja star(ip)

--- src/day2/TriangleShell.java
+++ src/day2/TriangleShell.java
@@ -5,225 +5,42 @@ import org.jointheleague.graphical.robot.Robot;
 public class TriangleShell {
 
 	// 1. Create a new Robot
-	Robot bob = new Robot();
+Robot bob = new Robot();
 
 	void go() {
-		angleShell {
-
-			// 1. Create a new Robot
-			Robot bob = new Robot();
-
-			void go() {
-
-					
-				// 6. Make the robot go as fast as possible
-				angleShell {
-
-					// 1. Create a new Robot
-					Robot bob = new Robot();
-
-					void go() {
-
-							
-						// 6. Make the robot go as fast as possible
-						angleShell {
-
-							// 1. Create a new Robot
-							Robot bob = new Robot();
-
-							void go() {
-
-									
-								// 6. Make the robot go as fast as possible
-								angleShell {
-
-									// 1. Create a new Robot
-									Robot bob = new Robot();
-
-									void go() {
-
-											
-										// 6. Make the robot go as fast as possible
-										angleShell {
-
-											// 1. Create a new Robot
-											Robot bob = new Robot();
-
-											void go() {
-
-													
-												// 6. Make the robot go as fast as possible
-										        bob.setSpeed(100);
-												/angleShell {
-
-													// 1. Create a new Robot
-													Robot bob = new Robot();
-
-													void go() {
-
-															
-														// 6. Make the robot go as fast as possible
-												        bob.setSpeed(100);
-												        angleShell {
-
-												        	// 1. Create a new Robot
-												        	Robot bob = new Robot();
-
-												        	void go() {
-
-												        			
-												        		// 6. Make the robot go as fast as possible
-												                bob.setSpeed(100);
-												        		// 4. make a variable to hold the length of the triangle and set it to 50
-												                angleShell {
-
-												                	// 1. Create a new Robot
-												                	Robot bob = new Robot();
-
-										                			
-											                		// 6. Make the robot go as fast as possible
-		
-										                			
-											                		// 6. Make the robot go as fast as possible
 		
 
-										                			
-											                		// 6. Make the robot go as fast as possible
-					                	void go() {
-
-												                			
-												                		// 6. Make the robot go as fast as possible
-												                        bob.setSpeed(100);
-												                		// 4. make a variable to hold the length of the triangle and set it to 50
-												                    	int length = 50;  
-												                		
-												                        // 7. Do the following (up to step 10) 60 times
-												                           
-												                		// 9. Change the color of the pen to a random color
-												                          
-													                		// 8. Increase the length variable by 10
-
-												                		// 5. call your drawTriangle() method using your length variable
-
-												                		// 10. Turn the robot 6 degrees to the right
-		            	int length = 50;  
-												        		
-												                // 7. Do the following (up to step 10) 60 times
-												                   
-												        		// 9. Change the color of the pen to a random color
-												                  
-												        		// 8. Increase the length variable by 10
-
-												        		// 5. call your drawTriangle() method using your length variable
-
-												        		// 10. Turn the robot 6 degrees to the right
-		// 4. make a variable to hold the length of the triangle and set it to 50
-												    	int length = 50;  
-														
-												        // 7. Do the following (up to step 10) 60 times
-												           
-														// 9. Change the color of the pen to a random color
-												          
-														// 8. Increase the length variable by 10
-
-														// 5. call your drawTriangle() method using your length variable
-
-														// 10. Turn the robot 6 degrees to the right
-/ 4. make a variable to hold the length of the triangle and set it to 50
-										    	int length = 50;  
-												
-										        // 7. Do the following (up to step 10) 60 times
-										           
-												// 9. Change the color of the pen to a random color
-										          
-												// 8. Increase the length variable by 10
-
-												// 5. call your drawTriangle() method using your length variable
-
-												// 10. Turn the robot 6 degrees to the right
-							        bob.setSpeed(100);
-										// 4. make a variable to hold the length of the triangle and set it to 50
-								    	int length = 50;  
-										
-								        // 7. Do the following (up to step 10) 60 times
-								           
-										// 9. Change the color of the pen to a random color
-								          
-										// 8. Increase the length variable by 10
-
-										// 5. call your drawTriangle() method using your length variable
-
-										// 10. Turn the robot 6 degrees to the right
-					        bob.setSpeed(100);
-								// 4. make a variable to hold the length of the triangle and set it to 50
-						    	int length = 50;  
-								
-						        // 7. Do the following (up to step 10) 60 times
-						           
-								// 9. Change the color of the pen to a random color
-						          
-								// 8. Increase the length variable by 10
-
-								// 5. call your drawTriangle() method using your length variable
-
-								// 10. Turn the robot 6 degrees to the right
-			        bob.setSpeed(100);
-						// 4. make a variable to hold the length of the triangle and set it to 50
-				    	int length = 50;  
-						
-				        // 7. Do the following (up to step 10) 60 times
-				           
-						// 9. Change the color of the pen to a random color
-				          
-						// 8. Increase the length variable by 10
-
-						// 5. call your drawTriangle() method using your length variable
-
-						// 10. Turn the robot 6 degrees to the right
-	        bob.setSpeed(100);
-				// 4. make a variable to hold the length of the triangle and set it to 50
-		    	int length = 50;  
-				
-		        // 7. Do the following (up to step 10) 60 times
-		           
-				// 9. Change the color of the pen to a random color
-		          
-				// 8. Increase the length variable by 10
-
-				// 5. call your drawTriangle() method using your length variable
-
-				// 10. Turn the robot 6 degrees to the right
-
-			
 		// 6. Make the robot go as fast as possible
-        bob.setSpeed(100);
+		 bob.setSpeed(100);
 		// 4. make a variable to hold the length of the triangle and set it to 50
-    	int length = 50;  
-		
+        int length = 50;
+        
         // 7. Do the following (up to step 10) 60 times
-           
-		// 9. Change the color of the pen to a random color
-          
-		// 8. Increase the length variable by 10
-
-		// 5. call your drawTriangle() method using your length variable
-
-		// 10. Turn the robot 6 degrees to the right
-
+        for(int i=0; i<60; i++) {
+			bob.turn(-6);
+        	// 9. Change the color of the pen to a random color
+	        bob.setRandomPenColor();
+			// 8. Increase the length variable by 10
+	        length=length+10;
+			// 5. call your drawTriangle() method using your length variable
+            drawTriangle(length);
+			// 10. Turn the robot 6 degrees to the right
+        
+        }
 	}
 
-	/*
-	 * 2. fill in the method below to draw a triangle. Use the length variable when
-	 * you call move().
-	 */
+	/* 2. fill in the method below to draw a triangle. Use the length variable when you call move(). */
 	private void drawTriangle(int length) {
-		for (int i = 0; i < 3; i++) {
-			bob.move(length);
-			bob.turn(120);
-			System.out.println(i);
-		}
+    for(int i=0; i < 3; i++) {
+	bob.penDown();	
+    bob.move(length);
+	bob.turn(120);		
+    }
+
+	
 	}
 
+	 
 	public static void main(String[] args) {
 		new TriangleShell().go();
 	}
